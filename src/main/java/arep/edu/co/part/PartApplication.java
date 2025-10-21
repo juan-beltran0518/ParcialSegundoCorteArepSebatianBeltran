bd42c7d45afa075db4b036ee599776609a099e77
unknown <juan.brodriguez@labinfo.is.escuelaing.edu.co>
2025-10-21 09:00:48 -0500

    Parcial Segundo Tercio Inicializacion con Spring initializr

---
+++ src/main/java/arep/edu/co/part/PartApplication.java
@@ -0,0 +1,13 @@
+package arep.edu.co.part;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartApplication.class, args);
+	}
+
+}
